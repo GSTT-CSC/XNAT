9655848256ac78c2ac227343fee1c6d55dd71d5f
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2022-10-24 20:24:12 +0100

    Update collate_lunit_data.py

--- project-processing/send-to-pacs/collate_lunit_data.py
+++ project-processing/send-to-pacs/collate_lunit_data.py
@@ -37,8 +37,8 @@ def extract_header_info(xnat_configuration: dict, original_data:pd.DataFrame = N
                     findings = get_lunit_header(experiment)
                 except ValueError:
                     logging.warning(f'Subject {subject} has no valid dicom resource!')
-                    results_list.append({'Subject': subject.label,
-                                        'EXCLUDE': True})
+                    results_list.append(pd.DataFrame({'Subject': [subject.label],
+                                        'EXCLUDE': [True]}).set_index('Subject'))
                     continue
                 if not findings:
                     logging.info(f'Subject {subject} has no findings')
