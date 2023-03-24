06abd01da7ac245e6ac05433187da13c7370960c
Dika Vilic <dijana.vilic@gstt.nhs.uk>
2023-03-24 15:56:54 +0000

    remove .sh
    There is no need to run from within docker

--- xnat-csc/bash/SeriesDescription_Extractor.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-LINE=$(docker ps | egrep xnat-web1 | cut -c1-12)
-echo "$LINE"
-PATH=$1
-OUTPUT=$2
-
-docker exec -i "$LINE" python3 /data/scripts/import-dqr/SeriesDescription_Extractor.py $PATH /data/scripts/import-dqr/$OUTPUT
