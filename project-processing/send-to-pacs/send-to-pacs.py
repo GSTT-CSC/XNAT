763a6f051470adffd38998b15abb4cd96758ead3
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2023-05-09 14:20:23 +0100

    Update send-to-pacs.py

--- project-processing/send-to-pacs/send-to-pacs.py
+++ project-processing/send-to-pacs/send-to-pacs.py
@@ -3,7 +3,7 @@ import logging
 import time
 import configparser
 
-
+from xnat.mixin import ImageScanData
 logging.basicConfig(level=logging.INFO)
 
 
@@ -44,43 +44,44 @@ def send_lunit_data(session, experiment, pacs):
     elif len([x for x in [scan.id for scan in experiment.scans.values()] if 'ORIGINAL' in experiment.scans[x].dicom_dump(fields='00080008')[0]['value']]) > 1:
         logging.info(f'\t\tExcluding due to multiple ORIGINAL images in {experiment}')
     else:
-        for scan in experiment.scans.values():
-            # n_attempts = 3
-            # attempt = 0
-            # while attempt <= n_attempts:
-            #     try:
-            #         logging.info(f'\t\tExporting scan to destination: {scan.uri} - attempt {attempt}')
-            #         response = session.put('/xapi/dqr/export/',
-            #                                query={'pacsId': pacs['id'], 'session': experiment.id, 'scansToExport': scan.id})
-            #         logging.debug(response)
-            #         time.sleep(delay)
-            #         break
-            #     except Exception as e:
-            #         attempt = attempt + 1
-            #         time.sleep(delay)
-            #         if attempt == n_attempts:
-            #             logging.info(f'\t\tFAILED : {scan.uri}')
-            #             with open("failed_sends.txt", "a") as myfile:
-            #                 myfile.write(scan.uri)
-            #         else:
-            #             continue
-            #     break
-            for attempt in range(3):
-                try:
-                    logging.info(f'\t\tExporting scan to destination: {scan.uri} - attempt {attempt}')
-                    response = session.put('/xapi/dqr/export/',
-                                       query={'pacsId': pacs['id'], 'session': experiment.id, 'scansToExport': scan.id})
-                    logging.debug(response)
-                    time.sleep(delay)
-                except:
-                    logging.info(f'\t\tFAILED RETRYING: {scan.uri}')
-                    time.sleep(delay)
+        for scan in experiment.scans:
+            if isinstance(scan,ImageScanData):
+                # n_attempts = 3
+                # attempt = 0
+                # while attempt <= n_attempts:
+                #     try:
+                #         logging.info(f'\t\tExporting scan to destination: {scan.uri} - attempt {attempt}')
+                #         response = session.put('/xapi/dqr/export/',
+                #                                query={'pacsId': pacs['id'], 'session': experiment.id, 'scansToExport': scan.id})
+                #         logging.debug(response)
+                #         time.sleep(delay)
+                #         break
+                #     except Exception as e:
+                #         attempt = attempt + 1
+                #         time.sleep(delay)
+                #         if attempt == n_attempts:
+                #             logging.info(f'\t\tFAILED : {scan.uri}')
+                #             with open("failed_sends.txt", "a") as myfile:
+                #                 myfile.write(scan.uri)
+                #         else:
+                #             continue
+                #     break
+                for attempt in range(3):
+                    try:
+                        logging.info(f'\t\tExporting scan to destination: {scan.uri} - attempt {attempt}')
+                        response = session.put('/xapi/dqr/export/',
+                                           query={'pacsId': pacs['id'], 'session': experiment.id, 'scansToExport': scan.id})
+                        logging.debug(response)
+                        time.sleep(delay)
+                    except Exception as e:
+                        logging.info(f'\t\tFAILED RETRYING: {scan.uri}')
+                        time.sleep(delay)
+                    else:
+                        break
                 else:
-                    break
-            else:
-                logging.info(f'\t\tFAILED all attempts - moving on {scan.uri}')
-                with open("failed_sends.txt", "a") as myfile:
-                    myfile.write(scan.uri + "\n")
+                    logging.info(f'\t\tFAILED all attempts - moving on {scan.uri}')
+                    with open("failed_sends.txt", "a") as myfile:
+                        myfile.write(scan.uri + "\n")
 
         # we failed all the attempts - deal with the consequences.
 
